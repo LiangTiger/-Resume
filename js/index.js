b033f00dad84f8b20112ef1c2c0ad73612f7a56a
LiangTiger <740959030@qq.com>
2017-11-29 20:18:17 +0800

    评论插件

--- js/index.js
+++ js/index.js
@@ -1,43 +1,66 @@
-$(function(){
-    $(window).scroll(function(){
-        top1=$("#aboutMe").offset().top-52;
-        top2=$("#demand").offset().top-100;
-        top3=$("#experience").offset().top-100;
-        top4=$("#technology").offset().top-100;
-        top5=$("#works").offset().top-100;
-        top6=$("#contact").offset().top-100;
-        var s=$(window).scrollTop()
-        s>top1?$("#nav-bar").addClass("fixed"):$("#nav-bar").removeClass("fixed")
-        if(s>top1&&s<top2){
+$(function () {
+    $(window).scroll(function () {
+        top1 = $("#aboutMe").offset().top - 52;
+        top2 = $("#technology").offset().top - 100;
+        top3 = $("#works").offset().top - 100;
+        top4 = $("#contact").offset().top - 100;
+        var s = $(window).scrollTop()
+        s > top1 ? $("#nav-bar").addClass("fixed") : $("#nav-bar").removeClass("fixed")
+        if (s > top1 && s < top2) {
             $('.nav>li').eq(0).addClass("active").siblings().removeClass("active")
-        }else if(s>top2&&s<top3){
+        } else if (s > top2 && s < top3) {
             $('.nav>li').eq(1).addClass("active").siblings().removeClass("active")
-        }
-        else if(s>top3&&s<top4){
+        } else if (s > top3 && s < top4) {
             $('.nav>li').eq(2).addClass("active").siblings().removeClass("active")
-        }
-        else if(s>top4&&s<top5){
+        } else if (s > top4) {
             $('.nav>li').eq(3).addClass("active").siblings().removeClass("active")
-        }
-        else if(s>top5&&s<top6){
-            $('.nav>li').eq(4).addClass("active").siblings().removeClass("active")
-        }else if(s>top6){
-            console.log(s)
-            console.log(top6)
-            $('.nav>li').eq(5).addClass("active").siblings().removeClass("active")
-        }
+        } 
 
     })
-    $('.nav>li').bind('click',function(){
-        var index=$(this).index();
+    $('.nav>li').bind('click', function () {
+        var index = $(this).index();
         console.log(index)
-        _top=$(".section").eq(index+1).offset().top;
+        _top = $(".section").eq(index + 1).offset().top;
         moveTo();
     })
-    function moveTo(){
+
+    function moveTo() {
         $("html,body").animate({
-            scrollTop:_top
-        },500)
+            scrollTop: _top
+        }, 500)
     }
-  
+    creatChart();
+    function creatChart() {
+        //Get context with jQuery - using jQuery's .get() method.
+        var ctx = $("#myChart").get(0).getContext("2d");
+       
+        //This will get the first returned node in the jQuery collection.
+        var myChart = new Chart(ctx, {
+            type: 'radar',
+            data: {
+                labels : ["HTML", "CSS", "Javascript", "jQuery", "node.js"],
+                datasets: [{
+                    label: '熟悉程度',
+                    data: [75, 75, 71, 70, 60],
+                    backgroundColor: [
+                        'rgba(54, 162, 235, 0.4)',
+                    ],
+                    borderColor: [
+                        'rgba(54, 162, 235, 1)',
+                    ],
+                    borderWidth: 1
+                },
+            ]
+            },
+            options: {
+                responsive: true,
+                scale: {
+                    ticks: {
+                      min: 0,
+                      max: 100
+                    }
+                  }
+            }
+        })
+    }
 })
