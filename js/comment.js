8d4e31f3c7ca08b6e820198ddd8f2203174ad515
LiangTiger <740959030@qq.com>
2017-11-30 13:24:25 +0800

    修复问题，提高性能

--- js/comment.js
+++ js/comment.js
@@ -13,10 +13,3 @@
         
         
 )();
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?e109b8d09cc4d272c0008f65723ed6e9";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
